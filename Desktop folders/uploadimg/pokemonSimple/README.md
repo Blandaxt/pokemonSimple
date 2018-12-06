66bddd91d37641d6dac68743ced91e3adce989e6
Blandaxt <resilient@resilients-MacBook-Pro-2.local>
2018-12-06 13:51:05 -0500

    merging conflicts

---
+++ Desktop folders/uploadimg/pokemonSimple/README.md
@@ -0,0 +1,18 @@
+# Simple Express Project
+A project that allows the user to post a comment, read each previous comment, rate each comment with thumbs up or down and delete any comment inside the app. Can also upload a file.
+
+**Link to project:** localhost:8000
+
+## How It's Made:
+
+**Tech used:** HTML, CSS, JavaScript, express and node.js
+
+I was able to build it using html as a starter code after downloading my node modules and JSON package using the terminal. Then move the html to a template engine called (ejs)embed javascipt for my html file. I also went to *mlab* to create my database. I then created a css file to modify my styling. Then i started to to create my server file base on node.js/express. Connected the *mlab* to my server by getting the *( mongodb://<dbuser>:<dbpassword>@ds125871.mlab.com:25871/xxxx )*. Then finally created a JavaScript file for my click events such as thumbs up and thumbs down. Finally i focused on adding my food api and intergrate it to my modules and server file.
+
+Then i added a form tag in my template that allowed file uploads.
+
+### Lessons Learned:
+
+I learned some new methods that relate to the express template. These methods that represent the Crud. Crud Represents create, read, update, and delete which are the four basic functions of persistent storage. I also learned more about the node_modules dependency that were useful like nodemon and figlet.
+
+Lastly, i understood more on how to the server interface with the database. I understood where to write my mongodb client URL, and understood how each change i make affects my other files.
---
+++ Desktop folders/uploadimg/pokemonSimple/server.js
@@ -0,0 +1,144 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const multer = require('multer')
+
+const storages = multer.diskStorage({
+  destination: function( req, file, cb){
+    cb(null, './uploads/')
+  },
+  filename: function(req, file, cb){
+    cb(null, new Date().toISOString() + file.originalName);
+  }
+})
+
+const fileFilter = (req, file, cb) => {
+  //reject a file
+  if(file.mimtype === 'image/jpeg' || file.mimetype === 'image/png'){
+  cb(null, true)
+
+  } else {
+  cb(new Error('message'))
+  cb(null, false)
+  }
+}
+
+const uploads = multer({
+  storage: storages,
+  limit: {
+  fileSize: 1024 * 1024 * 2
+  },
+  fileFilter: fileFilter
+});
+
+// const Product = require("../models/product")
+
+var db
+
+MongoClient.connect('mongodb://demo:demodemo1@ds125871.mlab.com:25871/bland', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(process.env.PORT || 8000, () => {
+    console.log('listening on 8000')
+  })
+})
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+
+//---------------------------------------
+// IMAGE CODE
+//---------------------------------------
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'public/images/uploads')
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+var upload = multer({storage: storage});
+
+app.post('/up', upload.single('file-to-upload'), (req, res, next) => {
+
+    insertDocuments(db, 'public/images/uploads/' + req.file.filename, () => {
+        db.close();
+        res.json({'message': 'File uploaded successfully'});
+    });
+});
+
+var insertDocuments = function(db, filePath, callback) {
+    var collection = db.collection('user');
+    collection.insertOne({'imagePath' : filePath }, (err, result) => {
+        //assert.equal(err, null);
+        callback(result);
+    });
+}
+//---------------------------------------
+// IMAGE CODE END
+//---------------------------------------
+
+
+app.get('/', (req, res) => {
+  db.collection('pokemon').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {messages: result})
+  })
+})
+
+// app.get('/react', (req, res) => {
+//   db.collection('messages').find().toArray((err, result) => {
+//     if (err) return console.log(err)
+//     res.json(result)
+//   })
+// })
+
+app.post('/messages', upload.single('productImage'), (req, res) => {
+  console.log(req.file);
+  db.collection('pokemon').save({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+app.put('/messages', (req, res) => {
+  db.collection('pokemon')
+  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+    $set: {
+      thumbUp:req.body.thumbUp + 1
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.put('/messages2', (req, res) => {
+  db.collection('pokemon')
+  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+    $set: {
+      thumbDown:req.body.thumbDown + 1
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.delete('/messages', (req, res) => {
+  db.collection('pokemon').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('Message deleted!')
+  })
+})
